2e3c7a25be44479dc889355cdc153ddcff0ec445
Zouhair96 <54177116+Zouhair96@users.noreply.github.com>
2022-12-25 20:40:11 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,12 @@
 terraform {
+  required_version = ">= 1.0.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.28.0"
+      version = ">= 4.9.0"
     }
   }
 }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 
 
 
