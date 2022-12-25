46c4015db5feb7ccba88e77b4efebf7d09621904
Zouhair96 <54177116+Zouhair96@users.noreply.github.com>
2022-12-25 21:14:19 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,26 @@
+provider "aws" {
+
+ # access_key = "AKIAYGCPNFK6X6PLDYHM"
+ # secret_key = "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"
+  region     = "us-east-1"
+}
+
 terraform {
-  required_version = ">= 1.0.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.9.0"
+      version = "~> 3.21"
     }
   }
+   # backend "s3" {
+    #region  = "us-east-1"
+    #key     = "environment/dev/terraform.tfstate"
+    #bucket  = "aws-terraform-gitlabci-bucket"
+  #}
+
 }
+
+data "aws_caller_identity" "current" {} # used for accesing Account ID and ARN
 
 
 
